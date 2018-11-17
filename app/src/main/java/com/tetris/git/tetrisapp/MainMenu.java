1143655b12c9cc4d62b9168cc8a5da36b74c209a
Ryan Lavin <Srwonderbread@gmail.com>
2018-11-17 10:33:20 -0700

    Stud function ;)

---
+++ app/src/main/java/com/tetris/git/tetrisapp/MainMenu.java
@@ -0,0 +1,17 @@
+package com.tetris.git.tetrisapp;
+
+public class MainMenu {
+    private Game game;
+
+    public Boolean hasClickedOption(){
+        return false;
+    }
+
+    public Boolean isActive(){
+        return true;
+    }
+
+    public MainMenu menu(){
+        return null;
+    }
+}
